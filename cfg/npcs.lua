aa32f1a2baf78998aa15eb84298ece0308003f75
Arthur Felipe Kraemer <arthurkx2@hotmail.com>
2022-01-13 17:20:23 -0300

    Adiciona: script de controles de entidades

---
+++ cfg/npcs.lua
@@ -0,0 +1,26 @@
+cfg = {}
+
+cfg.density = {
+	peds = 1.0,
+	vehicles = 0
+}
+
+cfg.peds = {
+  "s_m_y_cop_01",
+  "s_f_y_sheriff_01",
+  "s_m_y_sheriff_01",
+  "s_m_y_hwaycop_01",
+  "s_m_m_snowcop_01",
+  "s_m_m_paramedic_01",
+  "s_m_y_airworker",
+  "S_M_Y_MARINE_01",
+  "S_M_Y_MARINE_02",
+  "S_M_Y_MARINE_03",
+  "S_M_M_MARINE_01"
+}
+
+cfg.noguns = {
+}
+
+cfg.nodrops = {
+}
